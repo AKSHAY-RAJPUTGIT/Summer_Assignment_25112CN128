106cb9ec89c3f35bd23029a2b522b2cefc6fb958
Akshay Rajput <apni_github_email@gmail.com>
2026-06-07 10:11:27 +0530

    DAY-2 assignment is uploaded

---
+++ DAY-2/productOFdigit.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num, digit, product = 1;
+    cout << "Enter a number: ";
+    cin >> num;
+    while (num > 0) {
+        digit = num % 10;    
+        product = product * digit;
+        num = num / 10;      
+    }
+    cout<<product;
+    return 0;
+}
