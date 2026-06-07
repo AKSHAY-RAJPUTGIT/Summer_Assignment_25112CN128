34c9e4d45754e98e0d2a638feb73fa9b970bf000
Akshay Rajput <apni_github_email@gmail.com>
2026-06-07 09:51:39 +0530

    DAY-3 assignment is uploaded

---
+++ DAY-3/prime.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main() {
+  int n;
+  cout<<"Enter the number: ";
+  cin>>n;
+  if(n<2)
+  cout<<"not prime";
+for(int i=2;i<n;i++)
+{
+  if(n%i==0)
+  cout<<"not prime";
+}
+cout<<"prime";
+}
