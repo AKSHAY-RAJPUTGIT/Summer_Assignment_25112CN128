618807ebe2f4d37c8b16daf91041fc2b4f6aec13
Akshay Rajput <apni_github_email@gmail.com>
2026-06-15 20:19:24 +0530

    DAY-15 assginment is uploaded

---
+++ DAY-15/movezero.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    int arr[100];
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    int j = 0;
+    for(int i = 0; i < n; i++) {
+        if(arr[i] != 0) {
+            arr[j] = arr[i];
+            j++;
+        }
+    }
+    while(j < n) {
+        arr[j] = 0;
+        j++;
+    }
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
