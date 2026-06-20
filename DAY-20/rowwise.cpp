03ee40445da3ec19a1653855d3ee2788366830c7
Akshay Rajput <apni_github_email@gmail.com>
2026-06-20 20:25:13 +0530

    DAY-20 assginment is uploaded

---
+++ DAY-20/rowwise.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+void rowWiseSum(int arr[][3], int row, int col)
+{
+    for(int i = 0; i < row; i++)
+    {
+        int sum = 0;
+        for(int j = 0; j < col; j++)
+        {
+            sum += arr[i][j];
+        }
+        cout << "Sum of Row " << i + 1 << " = " << sum << endl;
+    }
+}
+int main()
+{
+    int arr[3][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+    rowWiseSum(arr, 3, 3);
+    return 0;
+}
